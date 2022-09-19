76e8a24433712f64d7341d6456bf625de0edae1e
Serg <Emmet.86@mail.ru>
2022-09-19 15:20:47 +0300

    Перевод в папку ДЗ

---
+++ Homwork01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("ВВедите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ int max = 0;
+if(b > a)
+{
+    max = b;
+   
+}
+else
+{
+    max = a;
+    
+}
+Console.Write("Максимальное значение: " + max);
