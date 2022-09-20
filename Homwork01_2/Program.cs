432c45d92109b505727a6cde291a72513202d064
Serg <Emmet.86@mail.ru>
2022-09-20 11:40:42 +0300

    Задача 2 с диаграммой

---
+++ Homwork01_2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+while (max < b)
+{
+ max = b;
+}
+while (max < c)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное значение: " + max);
