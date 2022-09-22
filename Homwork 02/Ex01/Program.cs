0cf2b0d85b59b10084c2dbba19a3dc24601b8d09
Serg <Emmet.86@mail.ru>
2022-09-22 16:00:41 +0300

    ДЗ 2 - две задачи

---
+++ Homwork 02/Ex01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Здравствуйте!!!");
+Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int c = a % 100;
+int v = c / 10;
+Console.WriteLine(v);
+
---
+++ Homwork 02/Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] array = {1,2,3,4,5,6,7};
+int n = array.Length;
+Console.WriteLine("Здравствуйте!!!");
+Console.Write("Введите цифру дня недели: ");
+int find = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+while(index < n)
+{
+
+if(array[^1] == find)
+{
+    Console.WriteLine("ВЫХОДНОЙ");
+    break;
+}
+
+if(array[^2] == find)
+{
+Console.WriteLine("ВЫХОДНОЙ");
+    break;
+}
+else
+{
+    Console.WriteLine("РАБОЧИЙ");
+    break;
+}
+}
+
