7c8cb863f2ad39f8217f51d7851850bc59bf1c04
Serg <Emmet.86@mail.ru>
2022-09-26 16:03:33 +0300

    Решение ДомРаб №3

---
+++ Homwork 03/Ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите проверяемое значение: ");
+string text = Console.ReadLine();
+
+
+bool Palindrom(string text)
+{
+for(int i = 0; i < text.Length/2; ++i)
+if(text[i] != text[text.Length - 1 -i]) return false;
+return  true;
+}
+bool proverks = Palindrom(text);
+Console.WriteLine(proverks);
---
+++ Homwork 03/Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("Введите значение числа:  ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+
+void Cube(int[] cube)
+{
+
+    int length = cube.Length;
+
+    for (int count = 0; count < length; count++)
+    {
+        cube[count] = Convert.ToInt32(Math.Pow(count, 3));
+
+    }
+}
+void PrintArr(int[] poz)
+{
+    int count1 = poz.Length;
+    for(int index = 1; index < count1; index++)
+    
+    {
+        Console.Write(poz[index] + "  ");
+        
+    }
+}
+
+int[] arry = new int[cube + 1];
+Cube(arry);
+PrintArr(arry);
