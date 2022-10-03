1e08e1bc9246a9feb25f9df00ef9a80675551c49
Serg <Emmet.86@mail.ru>
2022-10-03 15:19:10 +0300

    задача 2

---
+++ Homwork 04/Ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[10];
+int length = array.Length;
+int res = 0;
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(1, 30);
+
+    Console.Write(array[i] + " ");
+
+
+}
+for (int i = 1; i < length; i = i + 2)
+    res += array[i];
+Console.WriteLine();
+Console.WriteLine("Сумма элементов под нечетными индексами :  " + res);
+
+
